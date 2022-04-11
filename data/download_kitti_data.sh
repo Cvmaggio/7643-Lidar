2ecafeb991677ca43c3f563d99caa544a155311a
akim305 <akim305@gatech.edu>
2022-04-10 20:19:05 -0400

    creating kitti dir structure

--- data/download_kitti_data.sh
+++ data/download_kitti_data.sh
@@ -11,7 +11,9 @@ FILES=(
 
 
 
-# ~25min just to curl zips
+# ~25min to curl zips
+# ~10min to unzip
+# ~80GB total storage needed after unzipping
 for f in ${FILES[@]}; do
 
     echo "File: ${f}"
@@ -21,10 +23,46 @@ for f in ${FILES[@]}; do
         curl -s "https://s3.eu-central-1.amazonaws.com/avg-kitti/${f}.zip" -o "${f}.zip"
     fi
     
-    echo -e "\tunzipping..."
-    unzip -q -n "${f}.zip" -d "${f}"
+    
+    if [ ! -d "${f}" ]; then
+        echo -e "\tunzipping..."
+        unzip -q -n "${f}.zip" -d "${f}"
+    fi
     
     echo -e "\tdone"
 
 done
 
+
+
+
+
+mkdir -p kitti/ImageSets kitti/training kitti/testing
+
+
+
+wget -c https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/test.txt --no-check-certificate --content-disposition -O kitti/ImageSets/test.txt
+wget -c https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/train.txt --no-check-certificate --content-disposition -O kitti/ImageSets/train.txt
+wget -c https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/val.txt --no-check-certificate --content-disposition -O kitti/ImageSets/val.txt
+wget -c https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/trainval.txt --no-check-certificate --content-disposition -O kitti/ImageSets/trainval.txt
+
+
+
+
+mv data_object_calib/training/calib kitti/training/
+mv data_object_calib/testing/calib kitti/testing/
+
+mv data_object_image_2/training/image_2 kitti/training/
+mv data_object_image_2/testing/image_2 kitti/testing/
+
+mv data_object_velodyne/training/velodyne kitti/training/
+mv data_object_velodyne/testing/velodyne kitti/testing/
+
+mv data_object_label_2/training/label_2 kitti/training/
+
+
+
+# # delete now empty unzipped dirs
+# for f in ${FILES[@]}; do
+#     rm -rf "${f}"
+# fi
